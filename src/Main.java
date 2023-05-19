d7435f75517e61dbfd34597d5b9c298e1f2ba540
Pivkina <ketrin_696@mail.ru>
2023-05-19 23:18:05 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int x = 453;
+        int y = 1200;
+        if (y > 1000) {
+            System.out.println(y + y / 100 + x);
+        } else {
+            System.out.println(y + x);
+        }
+
+
+    }
+}
